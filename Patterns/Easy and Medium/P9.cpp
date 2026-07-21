e697001cdb7b623474ae146ba587cd53208d8d2c
Raja Kumar Bisi <143925079+Kumarraja25@users.noreply.github.com>
2026-07-21 02:18:58 +0000

    first 10 patten

---
+++ Patterns/Easy and Medium/P9.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5-i;j++){
+            cout<<" ";
+            
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=4;i>=1;i--){
+        for(int j=1;j<=5-i;j++){
+            cout<<" ";
+            
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
