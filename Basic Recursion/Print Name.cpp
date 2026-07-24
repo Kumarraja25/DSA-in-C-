5986c918eccf4802518a7078bd78f279dc5a8be3
Raja Kumar Bisi <143925079+Kumarraja25@users.noreply.github.com>
2026-07-24 12:33:11 +0000

    recursion initial problems

---
+++ Basic Recursion/Print Name.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void F(int i,int j){
+   if(i>j) return;
+   cout<<"Raja ";
+   F(i+1,j);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the no of iteration: ";
+    cin>>n;
+    F(1,n);
+    return 0;
+}
