64f110aa95a6d1161cd3e6c2fa7480f15f0bbc6a
Raja Kumar Bisi <143925079+Kumarraja25@users.noreply.github.com>
2026-08-14 14:37:01 +0530

    sum

---
+++ Basic Recursion/Sum 1-N (Functional).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Sum(int x){
+    if(x==0) return 0;
+    return x+Sum(x-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the no:";
+    cin>>n;
+    cout<<"Sum upto"<<n<<"is :"<<Sum(n);
+    return 0;
+}
