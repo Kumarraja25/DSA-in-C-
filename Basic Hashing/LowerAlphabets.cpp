8d47c241c0e574ee42858b51909a1813b17da2d0
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-08-19 19:43:43 +0530

    hash started

---
+++ Basic Hashing/LowerAlphabets.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the input array size: ";
+    cin>>n;
+    char arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+}
---
+++ Basic Hashing/hashArray.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,query;
+    cout<< "Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    // Precompute
+    int hash[13]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]]+=1;
+    }
+
+
+
+    // fetching
+    cout<< "How many numbers you want to check: ";
+    cin>>query;
+    while(query--){
+        int num;
+        cin>>num;
+        cout<<hash[num];
+    }
+
+
+}
