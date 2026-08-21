8ff156ae0d7e7964d99ead5ac3097561076a1305
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-08-21 15:42:56 +0530

    hashing complete

--- Basic Hashing/hashingUsingSTL.cpp
+++ Basic Hashing/hashingUsingSTL.cpp
@@ -20,14 +20,14 @@ int main(){
         cout<<i.first<<"->"<<i.second<<endl;
     }
 
-    // unordered_map<int,int> mpp1;
-    // for(int i=0;i<n;i++){
-    //     mpp1[arr[i]]++;
-    // }
+    unordered_map<int,int> mpp1;
+    for(int i=0;i<n;i++){
+        mpp1[arr[i]]++;
+    }
 
-    // for( auto i:mpp1){
-    //     cout<<i.first<<"->"<<i.second<<endl;
-    // }
+    for( auto i:mpp1){
+        cout<<i.first<<"->"<<i.second<<endl;
+    }
 
     int q;
     cout<<"Enter total numbers to check : ";
