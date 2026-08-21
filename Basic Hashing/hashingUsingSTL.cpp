7db907b5427c00825b8e1be187ffa331f68f4bb8
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-08-21 14:33:07 +0530

    hashing using stl

---
+++ Basic Hashing/hashingUsingSTL.cpp
@@ -0,0 +1,29 @@
+// Map is always a sorted STL
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n;
+    cout<<"Enter the no of inputs : ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    map<int,int> mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+
+    int q;
+    cout<<"Enter total numbers to check : ";
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        cout<<mpp[num]<<" ";
+    }
+    return 0;
+}
