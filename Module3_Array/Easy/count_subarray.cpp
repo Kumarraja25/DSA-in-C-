9a9dcc0dfbe9e162699716b6039e287a19e47b03
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-09-02 21:44:46 +0530

    count subarray

---
+++ Module3_Array/Easy/count_subarray.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarray_sum(vector<int> &a, int k){
+    map<long long,int> preSum;
+    int maxlen=0;
+    long long sum=0;
+    for(int i=0;i<a.size();i++){
+        sum+=a[i];
+        if(sum==k){
+            maxlen=max(maxlen,i+1);
+        }
+        long long rem=sum-k;
+        if(preSum.find(rem)!=preSum.end()){
+            int len=i-preSum[rem];
+            maxlen=max(maxlen,len);
+        }
+        if(preSum.find(sum)==preSum.end()){
+            preSum[sum]=i;
+        }
+    }
+    return maxlen;
+}
+int count_subarray(vector<int> &a, int k){
+    long long sum=a[0];
+    int maxlen=0;
+    int left=0, right=0;
+    int n=a.size();
+    int count=0;
+    while(right<n){
+        while(left<=right && sum>k){
+            sum-=a[left];
+            left++;
+        }
+        if(sum==k){
+            maxlen=max(maxlen,right-left+1);
+            count++;
+        }
+        right++; 
+        if(right<n) sum+=a[right];
+    }
+    return count;
+}
+
+int main(){
+    vector<int> v={1,2,3,1,1,1,1,3,3};
+    cout<<count_subarray(v,6);
+    return 0;
+}
