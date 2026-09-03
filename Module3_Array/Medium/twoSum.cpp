c9f813e5ce3ecef675cea4648b7ab00cda2b3029
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-09-03 18:39:38 +0530

    two sum

--- Module3_Array/Medium/twoSum.cpp
+++ Module3_Array/Medium/twoSum.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> twoSum(vector<int> &a,int target){
+vector<int> twoSum1(vector<int> &a,int target){
     int n=a.size();
     vector<int> b;
     for(int i=0;i<n-1;i++){
@@ -16,6 +16,40 @@ vector<int> twoSum(vector<int> &a,int target){
     return b;
     
 }
+
+vector<int> twoSum2(vector<int> &a,int target){
+    map<int,int> mpp;
+    vector<int> b;
+
+    for(int i=0;i<a.size();i++){
+        int rem=target-a[i];
+        if(mpp.find(rem)!=mpp.end()){
+            b.push_back(mpp[rem]);
+            b.push_back(i);
+            return b;
+        }
+        mpp[a[i]]=i;
+    }
+    return b;
+}
+vector<int> twoSum(vector<int> &a,int target){
+    vector<int> b;
+    sort(a.begin(),a.end());
+    int i=0;
+    int j=a.size()-1;
+    while(i<j){
+        int sum=a[i]+a[j];
+        if(sum>target) j--;
+        else if(sum<target) i++;
+        else{
+            b.push_back(i);
+            b.push_back(j);
+            break;
+        }
+    }
+    return b;
+}
+
 
 int main(){
     int n,k;
