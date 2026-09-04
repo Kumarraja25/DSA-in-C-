090ba49a6fea8a6c3d58f71cdce2f91bcab5f5b2
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-09-04 12:03:24 +0530

    majority brute

---
+++ Module3_Array/Medium/majority.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int majority(vector<int> &a){
+    for(int i=0;i<a.size();i++){
+        int count=0;
+        for(int j=0;j<a.size();j++){
+            if(a[j]==a[i]) count++;
+        }
+        if(count>(a.size()/2)){
+            return a[i];
+        }
+
+    }
+    return -1;
+        
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size: ";
+    cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int num;
+        cin>>num;
+        v.push_back(num);
+    }
+    cout<<"Majority element: "<<majority(v);
+    return 0;
+}
