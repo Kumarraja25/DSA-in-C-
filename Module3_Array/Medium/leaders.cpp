00f41cbdca2a0f00e4d6c760d280e6a4522f4d41
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-09-07 12:21:45 +0530

    leader updated

--- Module3_Array/Medium/leaders.cpp
+++ Module3_Array/Medium/leaders.cpp
@@ -1,19 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> leaders(vector<int> &a){
+vector<int> leaders1(vector<int> &a){
     int n=a.size();
     vector<int> result;
     for(int i=0;i<n;i++){
         int flag=true;
         for(int j=i+1;j<n;j++){
-            if(a[i]<a[j]){
+            if(a[i]<=a[j]){
                 flag=false;
                 break;
             }
         }
         if(flag==true){
             result.push_back(a[i]);
+        }
+    }
+    return result;
+}
+vector<int> leaders(vector<int> &a){
+    int n=a.size();
+    int max=INT_MIN;
+    vector<int> result;
+    for(int i=n-1;i>=0;i--){
+        if(a[i]>max){
+            result.push_back(a[i]);
+            max=a[i];
         }
     }
     return result;
