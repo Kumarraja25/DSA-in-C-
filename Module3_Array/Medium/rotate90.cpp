6414e240e87f1f25dbf6718cb5812a41160b3911
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-09-08 23:59:53 +0530

    rotate

--- Module3_Array/Medium/rotate90.cpp
+++ Module3_Array/Medium/rotate90.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void rotate(int* a,int size){
-    for(int i=0;i<size;i++){
-        for(int j=0;j<size;j++){
-            int temp=a[i*size+j];
-            a[i*size+j]=a[j*size+size-i-1];
-            a[j*size+size-i-1]=temp;
-        }
-    }
-}
+// void rotate(int* a,int size){
+//     for(int i=0;i<size;i++){
+//         for(int j=0;j<size;j++){
+//             int temp=a[i*size+j];
+//             a[i*size+j]=a[j*size+size-i-1];
+//             a[j*size+size-i-1]=temp;
+//         }
+//     }
+// }
 
 int main(){
     int n;
