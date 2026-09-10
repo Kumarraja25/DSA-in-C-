f33a4b7a8d0b2308ed6593152a849ac867ad5600
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-09-10 20:13:57 +0530

    rotate

--- Module3_Array/Medium/rotate90.cpp
+++ Module3_Array/Medium/rotate90.cpp
@@ -1,34 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// void rotate(int* a,int size){
-//     for(int i=0;i<size;i++){
-//         for(int j=0;j<size;j++){
-//             int temp=a[i*size+j];
-//             a[i*size+j]=a[j*size+size-i-1];
-//             a[j*size+size-i-1]=temp;
-//         }
-//     }
-// }
+void rotate1(vector<vector<int>> &a,int size){
+    int newArray[size][size];
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            newArray[j][size-i-1]=a[i][j];
+        }
+    }
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            a[i][j]=newArray[i][j];
+        }
+    }
+}
+void rotate(vector<vector<int>> &a,int size){
+    for(int i=0;i<size;i++){
+        for(int j=0;j<=i;j++){
+            swap(a[i][j],a[j][i]);
+        }
+    }
+    for(int i=0;i<size;i++){
+        reverse(a[i].begin(),a[i].end());
+    }
+}
 
 int main(){
     int n;
     cout<<"Enter the size: ";
     cin>>n;
-    int array[n][n];
-    
+    vector<vector<int>> v(n);
     for(int i=0;i<n;i++){
       for(int j=0;j<n;j++){
-        cin>>array[i][j];
+        int num;
+        cin>>num;
+        v[i].push_back(num);
       }   
-      cout<<"/n";
     }
-    rotate(array,n);
+    rotate(v,n);
+    cout<<"After rotation: \n";
     for(int i=0;i<n;i++){
       for(int j=0;j<n;j++){
-        cout<<array[i][j]<<" ";
+        cout<<v[i][j]<<"  ";
       }   
-      cout<<"/n";
+      cout<<"\n";
     }
     
 
