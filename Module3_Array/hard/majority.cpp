82cdf19c9badfccee0f954748bcab310db0e3194
Raja Kumar Bisi <rajakumarbisi2003@gmail.com>
2026-09-18 17:06:06 +0530

    majority completed

--- Module3_Array/hard/majority.cpp
+++ Module3_Array/hard/majority.cpp
@@ -18,15 +18,44 @@ vector<int> majority1(vector<int> &a){
     }
     return ls;
 }
-vector<int> majority(vector<int> &a){
+vector<int> majority2(vector<int> &a){
     int n=a.size();
     vector<int> ls;
     map<int,int> mpp;
     for(int i=0;i<n;i++){
         mpp[a[i]]++;
-        if(mpp[a[i]]==(n/2)+1) ls.push_back(a[i]);
-        if(ls.size()==2) break;
+        if(mpp[a[i]]==(n/3)+1) ls.push_back(a[i]);
     }
+    return ls;
+}
+vector<int> majority(vector<int> &a){
+    int n=a.size();
+    vector<int> ls;
+    int ele1,ele2;
+    int count1=0,count2=0;
+    for(int i=0;i<n;i++){
+        if(count1==0 && a[i]!=ele2){
+            ele1=a[i];
+            count1=1;
+        }
+        if(count2==0 && a[i]!=ele1){
+            ele2=a[i];
+            count2=1;
+        }
+        else if(ele1==a[i]) count1++;
+        else if(ele2==a[i]) count2++;
+        else{
+            count1--;
+            count2--;
+        }
+    }
+    count1=0,count2=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==ele1) count1++;
+        if(a[i]==ele2) count2++;
+    }
+    if(count1>=(n/3)+1) ls.push_back(ele1);
+    if(count2>=(n/3)+1) ls.push_back(ele2);
     return ls;
 }
 
